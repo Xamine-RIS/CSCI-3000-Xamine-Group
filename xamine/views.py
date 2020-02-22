4360c76fefb06ba7ba9c2bb17da7707b132aea60
matt burlage <mattburlage@gmail.com>
2020-02-22 13:04:26 -0500

    set up coming soon page

--- xamine/views.py
+++ xamine/views.py
@@ -2,3 +2,6 @@ from django.contrib.auth import authenticate
 from django.http import HttpResponseRedirect
 from django.shortcuts import render, redirect
 
+@login_required
+def index(request):
+    return render(request, 'base/coming_soon.html')
