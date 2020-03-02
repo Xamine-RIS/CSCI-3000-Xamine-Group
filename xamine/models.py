c425c666479ce1ee52bc5a25e0367ead108c8af3
mattburlage <mattburlage@gmail.com>
2020-03-02 15:22:46 -0500

    Update models.py

--- xamine/models.py
+++ xamine/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 
 class Patient(models.Model):
+    """ Model to track the patient and their history """
+    
     first_name = models.CharField(max_length=128)
     middle_name = models.CharField(max_length=128, blank=True, null=True)
     last_name = models.CharField(max_length=128)
@@ -14,3 +16,16 @@ class Patient(models.Model):
             
     def __str__(self):
         return f"{self.full_name} ({self.id})"
+
+class Order(models.Model):
+    """ Model for each individual imaging order placed by doctors """
+    
+    # link to patient to be images
+    patient = models.ForeignKey(Patient, on_delete=models.CASCADE, related_name="orders")
+    
+    # Automatically record timestamp info
+    added_on = models.DateTimeField(auto_now_add=True)
+    last_edit = models.DateTimeField(auto_now=True)
+    
+    def __str__(self):
+        return f"#{self.id} - {self.patient.full_name}"
