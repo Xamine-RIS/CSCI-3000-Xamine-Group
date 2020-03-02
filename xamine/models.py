2e9ab42d717e0f886a82d9ec08738b14ac44da73
mattburlage <mattburlage@gmail.com>
2020-03-02 14:40:42 -0500

    example of view rendering

--- xamine/models.py
+++ xamine/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
-# Create your models here.
+class Patient(models.Model):
+    first_name = models.CharField(max_length=128)
+    middle_name = models.CharField(max_length=128, blank=True, null=True)
+    last_name = models.CharField(max_length=128)
--- xamine/urls.py
+++ xamine/urls.py
@@ -3,6 +3,10 @@ from django.urls import path, include
 from xamine import views
 
 urlpatterns = [
+    path('patient/<int:patid>', views.index, name='index'),
+    path('', views.index, name='index'),
+    path('', views.index, name='index'),
+    path('', views.index, name='index'),
     path('', views.index, name='index'),
 
 ]
