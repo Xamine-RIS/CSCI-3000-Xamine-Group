11492453fdd64ffce148d57c6007f510928f11a1
mattburlage <mattburlage@gmail.com>
2020-03-02 15:06:02 -0500

    added full_name shortcut to get patient name

--- xamine/models.py
+++ xamine/models.py
@@ -4,3 +4,14 @@ class Patient(models.Model):
     first_name = models.CharField(max_length=128)
     middle_name = models.CharField(max_length=128, blank=True, null=True)
     last_name = models.CharField(max_length=128)
+    
+    @property
+    def full_name:
+        if self.middle_name:
+            return f"{self.first_name} {self.middle_name} {self.last_name}"
+        else:
+            return f"{self.first_name} {self.last_name}"  
+            
+     
+    def __str__(self):
+        return self.full_name
