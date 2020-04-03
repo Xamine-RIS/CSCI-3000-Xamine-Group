9d106c816d58d4189237dcb76a0fba264df33e7c
matt burlage <mattburlage@gmail.com>
2020-04-03 19:36:55 -0400

    added levels and users
    Signed-off-by: matt burlage <mattburlage@gmail.com>

--- xamineapp/settings.py
+++ xamineapp/settings.py
@@ -79,6 +79,7 @@ WSGI_APPLICATION = 'xamineapp.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
+# Local database
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
