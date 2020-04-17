86c82c9beaf47e47b552769add90a9c5fad45aca
matt burlage <mattburlage@gmail.com>
2020-04-17 19:07:08 -0400

    changed formatting
    Signed-off-by: matt burlage <mattburlage@gmail.com>

--- xamine/templatetags/phone_number.py
+++ xamine/templatetags/phone_number.py
@@ -5,6 +5,7 @@ register = template.Library()
 @register.filter(name='phone_number')
 def phone_number(number):
     """Convert a 10 character string into (xxx) xxx-xxxx."""
+    number = number.replace(' ', '').replace('-', '').replace('(', '').replace(')', '')
     first = number[0:3]
     second = number[3:6]
     third = number[6:10]
