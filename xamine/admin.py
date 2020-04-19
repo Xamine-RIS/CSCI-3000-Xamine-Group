a706de5a2d7c1f61521e97f2eb5158224b66eccb
matt burlage <mattburlage@gmail.com>
2020-04-19 18:40:23 -0400

    added inline for image attachment
    Signed-off-by: matt burlage <mattburlage@gmail.com>

--- xamine/admin.py
+++ xamine/admin.py
@@ -2,10 +2,22 @@ from django.contrib import admin
 
 from xamine.models import Patient, Level, AppSetting, Order, Image, ModalityOption, Team
 
+
+class ImageInline(admin.TabularInline):
+    model = Image
+    fields = ['label', 'image', 'added_on', 'user']
+    readonly_fields = ['added_on', 'user']
+
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    readonly_fields = ['added_on', 'last_edit']
+    inlines = [ImageInline]
+
+
 admin.site.register(Patient)
 admin.site.register(Level)
 admin.site.register(AppSetting)
-admin.site.register(Order)
-admin.site.register(Image)
-admin.site.register(ModalityOption)  # temp
+# admin.site.register(Image)
+admin.site.register(ModalityOption)
 admin.site.register(Team)
