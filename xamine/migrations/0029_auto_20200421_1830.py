2a4afa288bd569375d58ac7629e7279a66cf9111
matt burlage <mattburlage@gmail.com>
2020-04-24 14:55:36 -0400

    fixed migration files
    Signed-off-by: matt burlage <mattburlage@gmail.com>

--- xamine/migrations/0029_auto_20200421_1830.py
+++ xamine/migrations/0029_auto_20200421_1830.py
@@ -10,6 +10,12 @@ class Migration(migrations.Migration):
     ]
 
     operations = [
+        migrations.AlterField(
+            model_name='image',
+            name='order',
+            field=models.ForeignKey(null=True, on_delete=django.db.models.deletion.CASCADE, related_name='images',
+                                    to='xamine.Order'),
+        ),
         migrations.AddField(
             model_name='order',
             name='completed',
