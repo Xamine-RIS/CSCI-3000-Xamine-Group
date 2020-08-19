544da935de7adda523e0fbe91fa3999b36ef08cc
mattburlage <mattburlage@gmail.com>
2020-08-19 08:44:14 -0400

    💄

--- xamineapp/prodsettings.py
+++ xamineapp/prodsettings.py
@@ -5,11 +5,3 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
-
-# Email Settings: -- Uncomment this to use your company's email server
-# EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
-# EMAIL_HOST = "smtp.gmail.com""
-# EMAIL_USE_TLS = True
-# EMAIL_PORT = 587
-# EMAIL_HOST_USER = "your_account@gmail.com"
-# EMAIL_HOST_PASSWORD = "your account’s password"
